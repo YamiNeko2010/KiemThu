0199a145ecabd81982c0bd6db1392c705117bce4
YamiNeko2010 <ducembe2010@gmail.com>
2024-04-23 15:41:28 +0700

    test

--- TriangleTypeCheckerTest.java
+++ TriangleTypeCheckerTest.java
@@ -9,29 +9,34 @@ public class TriangleTypeCheckerTest {
     public void testEquilateral() {
         String result = triangleTypeChecker.checkTriangleType(2, 2, 2);
         Assertions.assertEquals("Equilateral", result);
+        System.out.println("Equilateral: " + result);
     }
 
     @Test
     public void testIsosceles() {
         String result = triangleTypeChecker.checkTriangleType(2, 2, 3);
         Assertions.assertEquals("Isosceles", result);
+        System.out.println("Isosceles: " + result);
     }
 
     @Test
     public void testScalene() {
         String result = triangleTypeChecker.checkTriangleType(2, 3, 4);
         Assertions.assertEquals("Scalene", result);
+        System.out.println("Scalene: " + result);
     }
 
     @Test
     public void testRightTriangle() {
         String result = triangleTypeChecker.checkTriangleType(3, 4, 5);
         Assertions.assertEquals("RightTriangle", result);
+        System.out.println("RightTriangle: " + result);
     }
 
     @Test
     public void testNotATriangle() {
         String result = triangleTypeChecker.checkTriangleType(0, 1, 2);
         Assertions.assertEquals("NotATriangle", result);
+        System.out.println("NotATriangle: " + result);
     }
 }
